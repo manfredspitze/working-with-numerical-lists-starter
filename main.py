b5636a0be88542694ec0442064f3df726798a5bc
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-06 06:18:03 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# Student name
+# Current date
+# Project title
+
+# Part 1
+# Comment
+# Comment
+
+
+
+
+# Part 2
+# Comment
+# Comment
+
+
+
+
+# Part 3
+# Comment
+# Comment
+
+
+
+
+# Part 4
+# Comment
+# Comment
+
+
+
+
+# Part 5
+# Comment
+# Comment
