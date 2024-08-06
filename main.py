011f3553385a2db3ad7a0101dd6c9df1215e0ad8
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-06 06:27:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 # Student name
 # Current date
-# Project title
+# Working with Numerical Lists in Python
+
+# See pp. 57 - 59 in the Python Crash Course book first!
 
 # Part 1
-# Comment
-# Comment
+# Use a for loop and the range ( ) function to
+# generate and display the numbers 0 to 10
+
+# Hint: The output for the range ( ) function stops when it reaches the second value you provide, so in
+# this case, the last number generated will be 9
+# How do you change your range ( ) function code so it actually displays the number 10?
+
 
 
 
 
 # Part 2
-# Comment
-# Comment
+# Use a for loop and the range ( ) function to
+# generate and display the numbers 1 to 50
 
 
 
 
 # Part 3
-# Comment
-# Comment
+# Use the list ( ) function and the range ( ) function together 
+# to generate a list of numbers from 1 to 20
 
 
 
 
 # Part 4
-# Comment
-# Comment
+# Use the list ( ) function and the range ( ) function together 
+# to generate a list of ODD numbers from 1 to 30
+# Hint: You'll need to include a STEP VALUE in your range ( ) function
 
 
 
 
 # Part 5
-# Comment
-# Comment
+# Create an empty list named cubes
+# Then use a for loop and the range ( ) function to generate
+# and add to the list the cube of each number 
+# Hint: You'll also use the append ( ) method for this script
+# See page 58 in the Python Crash Course book
